4aae9121457b955905ea33e7b4ea11fb84b7a6c2
DonnyStout <donnystout47@gmail.com>
2018-03-25 00:55:30 -0600

    added more options to calories adapter when creating account

--- app/src/main/java/layout/CreateAccountFragment.java
+++ app/src/main/java/layout/CreateAccountFragment.java
@@ -130,10 +130,11 @@ public class CreateAccountFragment extends Fragment {
   }
 
 
-  public void adapter() {
+  private void adapter() {
     SpinnerAdapter caloriesAdapter = new ArrayAdapter<>(getActivity(),
         android.R.layout.simple_spinner_item,
-        Arrays.asList(1200, 1500, 2000, 2500, 3000, 3500));
+        Arrays.asList(1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200,
+            2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200, 3300, 3400, 3500));
     caloriesbox.setAdapter(caloriesAdapter);
   }
 
